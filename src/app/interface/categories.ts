e19fb9d0390302634e939aa8a2c018b74bb46211
Duy-Hiep <106466621+Duy-Hiep@users.noreply.github.com>
2023-06-05 01:31:21 +0700

    feature/DT-19-AddFileInterface

---
+++ src/app/interface/categories.ts
@@ -0,0 +1,5 @@
+export interface ICategory {
+    _id?: string,
+    name: string,
+    image: string
+}
---
+++ src/app/interface/product.ts
@@ -0,0 +1,7 @@
+export interface IProduct {
+    _id?: number,
+    name: string,
+    image: string,
+    price: number,
+    description: string
+}
