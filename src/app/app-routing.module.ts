15515c1183e2dbd61dc1d7ad69c658b03145ebe2
nguyenhongson <nguyenhongson20433@gmail.com>
2023-06-04 15:46:08 +0700

    feature/DT-23-BuildRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/Client/home-page/home-page.component';
+import { LayoutClientComponent } from './layout/layout-client/layout-client.component';
+import { LayoutAdminComponent } from './layout/layout-admin/layout-admin.component';
+import { DashboardComponent } from './pages/Admin/dashboard/dashboard.component';
+import { ProductManagerComponent } from './pages/Admin/product-manager/product-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: LayoutClientComponent, children:[
+      {
+        path: "", component: HomePageComponent
+      }
+    ]
+  },
+  {
+    path: "admin", component: LayoutAdminComponent, children:[
+      {
+        path: "", redirectTo:"dashboard", pathMatch:"full"
+      },
+      {
+        path: "dashboard", component: DashboardComponent
+      },
+      {
+        path: "product", component: ProductManagerComponent
+      }
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
