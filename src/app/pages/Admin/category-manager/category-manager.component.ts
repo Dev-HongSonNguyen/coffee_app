2513c290921d7158a1b0931f86a1a658f3bccfb6
Duy-Hiep <106466621+Duy-Hiep@users.noreply.github.com>
2023-06-06 00:24:12 +0700

    feature/DT-31-ListCategoryPageAdmin

---
+++ src/app/pages/Admin/category-manager/category-manager.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CategoryService } from 'src/app/service/category.service';
+
+@Component({
+  selector: 'app-category-manager',
+  templateUrl: './category-manager.component.html',
+  styleUrls: ['./category-manager.component.css']
+})
+export class CategoryManagerComponent {
+  categoryData: any
+  constructor(private categoryService: CategoryService){}
+  ngOnInit(){
+    this.categoryService.getAllCategory().subscribe((data: any) => {
+      this.categoryData = data.category
+    })
+  }
+}
